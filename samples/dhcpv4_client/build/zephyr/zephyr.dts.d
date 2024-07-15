488845957be2e596c1cd2efdcc776caf444d3ef8
ehughes <ehughes@wavenumber.net>
2024-07-15 10:06:25 -0400

    adding sample for dhcp

---
+++ samples/dhcpv4_client/build/zephyr/zephyr.dts.d
@@ -0,0 +1,24 @@
+empty_file.o: C:/ELI/mcxn-iot/zephyr/misc/empty_file.c \
+ C:/ELI/mcxn-iot/zephyr/boards/nxp/frdm_mcxn947/frdm_mcxn947_mcxn947_cpu0.dts \
+ C:/ELI/mcxn-iot/zephyr/dts/arm/nxp/nxp_mcxn94x.dtsi \
+ C:/ELI/mcxn-iot/zephyr/dts/common/mem.h \
+ C:/ELI/mcxn-iot/zephyr/dts/arm/armv8-m.dtsi \
+ C:/ELI/mcxn-iot/zephyr/dts/common/skeleton.dtsi \
+ C:/ELI/mcxn-iot/zephyr/dts/arm/nxp/nxp_mcxn94x_common.dtsi \
+ C:/ELI/mcxn-iot/zephyr/include/zephyr/dt-bindings/clock/mcux_lpc_syscon_clock.h \
+ C:/ELI/mcxn-iot/zephyr/include/zephyr/dt-bindings/gpio/gpio.h \
+ C:/ELI/mcxn-iot/zephyr/include/zephyr/dt-bindings/memory-attr/memory-attr-arm.h \
+ C:/ELI/mcxn-iot/zephyr/include/zephyr/sys/util_macro.h \
+ C:/ELI/mcxn-iot/zephyr/include/zephyr/sys/util_internal.h \
+ C:/ELI/mcxn-iot/zephyr/include/zephyr/sys/util_loops.h \
+ C:/ELI/mcxn-iot/zephyr/include/zephyr/sys/util_listify.h \
+ C:/ELI/mcxn-iot/zephyr/include/zephyr/sys/util_internal_is_eq.h \
+ C:/ELI/mcxn-iot/zephyr/include/zephyr/sys/util_internal_util_inc.h \
+ C:/ELI/mcxn-iot/zephyr/include/zephyr/sys/util_internal_util_dec.h \
+ C:/ELI/mcxn-iot/zephyr/include/zephyr/sys/util_internal_util_x2.h \
+ C:/ELI/mcxn-iot/zephyr/include/zephyr/dt-bindings/memory-attr/memory-attr.h \
+ C:/ELI/mcxn-iot/zephyr/boards/nxp/frdm_mcxn947/frdm_mcxn947.dtsi \
+ C:/ELI/mcxn-iot/zephyr/boards/nxp/frdm_mcxn947/frdm_mcxn947-pinctrl.dtsi \
+ C:/ELI/mcxn-iot/modules/hal/nxp/dts/nxp/mcx/MCXN947VDF-pinctrl.h \
+ C:/ELI/mcxn-iot/zephyr/include/zephyr/dt-bindings/i2c/i2c.h \
+ C:/ELI/mcxn-iot/zephyr/include/zephyr/dt-bindings/input/input-event-codes.h
